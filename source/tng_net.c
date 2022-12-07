a07ab7a64fc384a19a16bd63269820b6085a9e43
adean <8dino2@gmail.com>
2022-12-07 15:28:52 -0500

    Another try

--- source/tng_net.c
+++ source/tng_net.c
@@ -35,13 +35,13 @@ Init libcurl and multi handle.
 void cURL_Init(void)
 {
     if (curl_global_init(CURL_GLOBAL_NOTHING)) {
-            Com_Printf("curl_global_init failed\n");
-            return;
-        }
-
-        curl_initialized = true;
-        Com_Printf("%s initialized.\n", curl_version());
+        Com_Printf("curl_global_init failed\n");
+        return;
     }
+
+    curl_initialized = true;
+    Com_Printf("%s initialized.\n", curl_version());
+}
 
 void cURL_Shutdown(void)
 {
@@ -59,72 +59,39 @@ static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
 
 void cURL_MultiSend(void)
 {
-    // Do nothing if we don't have a handle to process
-    if(handle_count < 1){
-        return;
-    }
-    struct curl_slist *headerlist = NULL;
-
     CURL *curl = curl_easy_init();
     CURLM *multi_handle;
     multi_handle = curl_multi_init();
 
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Expect:");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    // Do nothing if we don't have a handle to process
+    gi.dprintf("Handle count is %i", handle_count);
+    if(handle_count < 1){
+        return;
+    }
+
+    // !!!
+    // Debug area //
+    // Do not print responses from curl request
+    // Comment this line...
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    // ...and uncomment this line for full debug mode
+    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    // End Debug //
+    
     if(curl && multi_handle) {
         curl_multi_add_handle(multi_handle, curl);
         curl_multi_perform(multi_handle, &handle_count);
         /* always cleanup */
         curl_multi_cleanup(multi_handle);
         curl_easy_cleanup(curl);
-        curl_slist_free_all(headerlist);
+        curl_slist_free_all(headers);
         handle_count--;
     }
-}
-
-int cURL_AddHandler(const char *url, const char *jsonmsg)
-{
-    CURL *curl = curl_easy_init();
-    CURLM *multi_handle;
-    multi_handle = curl_multi_init();
-
-    if(curl && multi_handle) {
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Expect:");
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-
-        /*
-        cURL options:
-            CURLOPT_URL - Endpoint to send data
-            CURLOPT_CUSTOMREQUEST - POST method
-            CURLOPT_HTTPHEADER - Informing the endpoint payload is JSON
-            CURLOPT_POSTFIELDS - Content (important part)
-            CURLOPT_TIMEOUT - 1 second timeout and give up
-            CURLOPT_FAILONERROR - Send will fail if response code is >=400
-
-            Debug options:
-            CURLOPT_WRITEFUNCTION - Sends response to a blackhole, to not 
-                clutter logs/console.  Comment to see full response.
-            CURLOPT_VERBOSE - Enables more verbose output, pair with above
-        */
-
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonmsg);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1L);
-        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
-
-        // !!!
-        // Debug area //
-        // Do not print responses from curl request
-        // Comment this line...
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        // ...and uncomment this line for full debug mode
-        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-        // End Debug //
-    }
-    handle_count++;
-    cURL_MultiSend();
-    return 0;
 }
 
 int cURL_SendMsg(int payloadType, const char *payload, ...)
@@ -141,7 +108,10 @@ int cURL_SendMsg(int payloadType, const char *payload, ...)
 	}
     
     /// So far so good, time to go to work
-    
+    CURL *curl = curl_easy_init();
+    CURLM *multi_handle;
+    multi_handle = curl_multi_init();
+
     va_start (argptr, payload);
     vsnprintf (text, sizeof(text), payload, argptr);
     va_end (argptr);
@@ -189,12 +159,19 @@ int cURL_SendMsg(int payloadType, const char *payload, ...)
         else 
     {
         // Invalid payloadType supplied, do nothing
-        gi.dprintf("cURL_Easy_Send error: invalid payloadType %i", payloadType);
+        gi.dprintf("cURL_SendMsg error: invalid payloadType %i", payloadType);
         return 0;
     }
     
     // Send this off to be consumed by curl
-    cURL_AddHandler(url, jsonmsg);
+    
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonmsg);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1L);
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+
+    curl_multi_add_handle(multi_handle, curl);
 
     return 0;
 }
