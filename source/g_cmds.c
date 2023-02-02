fd17adcf6b0c5fe9a2c13747549b90fd2bc3ea86
adean <8dino2@gmail.com>
2023-02-02 15:07:49 -0500

    Basic test

--- source/g_cmds.c
+++ source/g_cmds.c
@@ -1469,6 +1469,7 @@ void Cmd_Say_f (edict_t * ent, qboolean team, qboolean arg0, qboolean partner_ms
 		gi.cprintf (NULL, PRINT_CHAT, "%s", text);
 		if ((!team) && (!partner_msg)) {
 			IRC_printf (IRC_T_TALK, "%s", text);
+			runpython();
 		}
 	}
 	
